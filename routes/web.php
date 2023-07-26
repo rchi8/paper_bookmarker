ca11437030a5361c6d265401f45913c22aeb8c4f
ryohei8 <ryohei.chihara.web@gmail.com>
2023-07-26 23:47:44 +0900

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewController;
+use App\Http\Controllers\BookmarkController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () { return view('contents.index'); });
+Route::get('/', [ViewController::class, 'index'])->name('index');
+Route::get('/new', [ViewController::class, 'new'])->name('new');
+Route::get('/search', [ViewController::class, 'search'])->name('search');
+Route::get('/favorite', [ViewController::class, 'favorite'])->name('favorite');
+Route::post('/search_paper', [ViewController::class, 'searchPaper']);
+Route::post('/bookmark', [BookmarkController::class, 'bookmark']);
+Route::post('/delete', [BookmarkController::class, 'delete'])->name('delete');
+Route::get('/search_bookmark', [BookmarkController::class, 'search']);
+Route::post('/update_memo', [BookmarkController::class, 'updateMemo']);
+    
